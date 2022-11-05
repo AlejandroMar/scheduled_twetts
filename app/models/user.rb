81cb73bdc41a7ac4c2ad58d65835d432e6b66b78
alejo <lawebdealejo@gmail.com>
2022-11-05 14:52:38 -0500

    adds email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
+# email:string
+# password_digest:string
+
+#password:string virtual
+#password_confirmation:string virtual
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "must be valid email address" }
 end
