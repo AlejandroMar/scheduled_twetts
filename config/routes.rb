10f0e70d3e028debe74a932a8dc2b83a8dc5f6a9
alejo <lawebdealejo@gmail.com>
2022-11-10 15:31:12 -0500

    starts adding reset password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: "main#index"
+
   get "about-us", to: "about#index", as: :about
 
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
@@ -12,4 +16,5 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
+
 end
